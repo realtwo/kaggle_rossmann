d57fd62982b8ed1e65b23d31fcbf680d52d2e3e8
Fred <fred.chou.nd@gmail.com>
2016-06-17 17:09:11 +0800

    added grid search

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ import preprocess
 from sklearn.metrics import mean_squared_error, make_scorer
 from time import time
 
-from sklearn.cross_validation import cross_val_score
 
 seed = 53
 
@@ -10,19 +9,13 @@ def performance_metric(y_true, y_pred):
     return mean_squared_error(y_true, y_pred)**0.5
 
 
-def fit_and_predict(clf, x_train, y_train, x_test):
-    clf.fit(x_train, y_train)
-    y_pred = clf.predict(x_test)
-    return y_pred
-
-
 def evaluate_model(clf, x_train, y_train, x_test, y_test):
     print '----------------------'
     print clf
 
     start = time()
-    
-    y_pred = fit_and_predict(clf, x_train, y_train, x_test)
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
     print "RMSE: {}".format(performance_metric(y_test, y_pred))
 
     end = time() 
@@ -30,13 +23,10 @@ def evaluate_model(clf, x_train, y_train, x_test, y_test):
 
 
 
-def basic_training(x_train, x_test, y_train, y_test):
+def basic_training(clf, x_train, x_test, y_train, y_test):
   
     print 'Training basic models... '
 
-    # Learning models
-    from sklearn.ensemble import RandomForestRegressor
-    clf = RandomForestRegressor(random_state=seed)
     evaluate_model(clf, x_train, y_train, x_test, y_test)
 
     print 'Done!'  
@@ -52,17 +42,30 @@ def cal_baseline(x_train, x_test, y_train, y_test):
     print 'Baseline: median sales'
     print "RMSE: {}".format(performance_metric(y_true=y_test, y_pred=x_test[:, 6]))  #col 6 for median
 
-def tuning():
+def tuned_training(clf, x_train, x_test, y_train, y_test):
+    from sklearn.cross_validation import KFold
+    from sklearn.grid_search import GridSearchCV
+    
+    start = time()
+    print 'Tuning model...'
+
+    params = {'max_features':['auto', 'sqrt'],
+              'min_samples_leaf':[1, 10, 50],
+              'n_estimators':[5,10,20]}
+
     # cross validation
-    #score_func = make_scorer(performance_metric, greater_is_better=False)
-    #print "Cross validating..."
-    #scores = cross_val_score(clf, x_train, y_train, cv=3, scoring=score_func)
-    #print "CV result: RMSE: {}".format(scores)
+    cv_sets = KFold(x_train.shape[0], 3, True, seed)
+    score_func = make_scorer(performance_metric, greater_is_better=False)
+    print "Grid search..."
+    grid = GridSearchCV(clf, params, cv=cv_sets, scoring=score_func)
+    grid = grid.fit(x_train, y_train)
+    clf_opt = grid.best_estimator_  
 
-    pass
-
-
-
+    # predict with optimal model after fitting the data
+    y_pred = clf_opt.predict(x_test)
+    print "RMSE (tuned model): {}".format(performance_metric(y_test, y_pred))
+    end = time() 
+    print "Trained model in {:.4f} seconds".format(end - start) 
 
 def main():  
     x_all, y_all = preprocess.build_feature_label()
@@ -72,11 +75,17 @@ def main():
     x_train, x_test, y_train, y_test = train_test_split(x_all, y_all, 
                                          test_size=0.3, random_state=seed)
 
-    cal_baseline(x_train, x_test, y_train, y_test)
+    #cal_baseline(x_train, x_test, y_train, y_test)
 
+    # Learning models
+    from sklearn.ensemble import RandomForestRegressor
+    clf = RandomForestRegressor(random_state=seed)
 
-    basic_training(x_train, x_test, y_train, y_test)
+    #basic_training(clf, x_train, x_test, y_train, y_test)
   
+    tuned_training(clf, x_train, x_test, y_train, y_test)
+
+
 
 
 if __name__ == "__main__":
