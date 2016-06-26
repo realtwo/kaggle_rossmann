e6357b3921297a931010dcf2877712bb7d7fe42c
= <fred.chou.nd@gmail.com>
2016-06-26 17:14:40 +0800

    added sensitiviy analysis

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import preprocess
 from sklearn.metrics import mean_squared_error, make_scorer
 from time import time
+from sklearn.cross_validation import train_test_split
 
 
 seed = 53
@@ -8,8 +9,8 @@ seed = 53
 def performance_metric(y_true, y_pred):
     return mean_squared_error(y_true, y_pred)**0.5
 
-
-def evaluate_model(clf, x_train, y_train, x_test, y_test):
+def basic_training(clf, x_train, x_test, y_train, y_test):
+  
     print '----------------------'
     print 'Basic training'
     print clf
@@ -17,18 +18,11 @@ def evaluate_model(clf, x_train, y_train, x_test, y_test):
     start = time()
     clf.fit(x_train, y_train)
     y_pred = clf.predict(x_test)
+
     print "RMSE: {}".format(performance_metric(y_test, y_pred))
 
     end = time() 
     print "Trained model in {:.4f} seconds".format(end - start) 
-
-
-
-def basic_training(clf, x_train, x_test, y_train, y_test):
-  
-    print 'Training basic models... '
-
-    evaluate_model(clf, x_train, y_train, x_test, y_test)
 
     print 'Done!'  
 
@@ -48,11 +42,6 @@ def grid_search(clf, x_train, y_train, params):
     from sklearn.cross_validation import KFold
     from sklearn.grid_search import GridSearchCV
 
-#    params = {'max_features':['auto', 'sqrt'],
-#              'min_samples_leaf':[1, 10, 50],
-#              'n_estimators':[5,10,20]}
-
-
     # cross validation
     cv_sets = KFold(x_train.shape[0], 3, True, seed)
     score_func = make_scorer(performance_metric, greater_is_better=False)
@@ -68,32 +57,66 @@ def tuned_training(clf, x_train, x_test, y_train, y_test):
     print 'Tuning model...'
     
     p1 = 'max_depth'
-    params = {p1:[16,20,24]}
-    
+    params = {  p1: [16,20,24] }
+
     clf_opt = grid_search(clf, x_train, y_train, params)
 
     print "Parameter {} is {} for the optimal model.".format(p1, clf_opt.get_params()[p1])   
 
     # predict with optimal model after fitting the data
     y_pred = clf_opt.predict(x_test)
-    print "RMSE (tuned model): {}".format(performance_metric(y_test, y_pred))
+    rmse = performance_metric(y_test, y_pred)
+    print "RMSE (tuned model): {}".format(rmse)
     end = time() 
     print "Trained model in {:.4f} seconds".format(end - start) 
+    return rmse
+
+
+def cal_sensitivity(clf, x_all, y_all, train_size):
+
+    print '----------------------------'
+    print 'Sensitivity analysis'
+    rmse = []
+    test_data = x_all[:6]
+    test_result = y_all[:6]
+
+    print 'Feature for price prediction:'
+    print test_data
+    print 'True sales price:'
+    print test_result
+
+    x_all = x_all[6:]
+    y_all = y_all[6:]
+
+    p1 = 'max_depth'
+    params = { p1: [16,20,24] }
+
+    for v in xrange(0,10):
+        x_train, x_test, y_train, y_test = train_test_split(x_all, y_all, 
+                                         train_size=train_size, random_state=v)
+        clf_opt = grid_search(clf, x_train, y_train, params)
+        y_pred = clf_opt.predict(test_data)
+        print 'Sale prediction for run #{}: {}'.format(v, y_pred)
+        
+        y_pred = clf_opt.predict(x_test)
+        rmse.append(performance_metric(y_test, y_pred))
+
+    print 'RSME for 10 random splits: {}'.format(rmse)
+
+
 
 def main():  
     x_all, y_all = preprocess.build_feature_label()
 
 
-    from sklearn.cross_validation import train_test_split
     x_train, x_test, y_train, y_test = train_test_split(x_all, y_all, 
                                          test_size=0.3, random_state=seed)
 
-    #cal_baseline(x_train, x_test, y_train, y_test)
+    train_size = x_train.shape[0]
+
+    cal_baseline(x_train, x_test, y_train, y_test)
 
     # Learning models
-    #from sklearn.ensemble import RandomForestRegressor
-    #clf = RandomForestRegressor(random_state=seed)
-
     from sklearn.tree import DecisionTreeRegressor
     clf = DecisionTreeRegressor(random_state=seed)
 
@@ -102,7 +125,7 @@ def main():
   
     tuned_training(clf, x_train, x_test, y_train, y_test)
 
-
+    cal_sensitivity(clf, x_all, y_all, train_size)
 
 
 if __name__ == "__main__":
